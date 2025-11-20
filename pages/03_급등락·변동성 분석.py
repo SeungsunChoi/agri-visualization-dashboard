1d5558d2e6b66ef1e519837317a2321f3e3557ab
SeungsunChoi <spdlqjf0389@naver.com>
2025-11-20 23:37:05 +0900

    03_급등락·변동성 분석.py

--- pages/03_급등락·변동성 분석.py
+++ pages/03_급등락·변동성 분석.py
@@ -132,13 +132,13 @@ base_line = (
 
 spike_up_chart = (
     alt.Chart(spike_up)
-    .mark_circle(size=55, color="rgba(255,0,0,0.60)")
+    .mark_circle(size=40, color="rgba(255,0,0,1)")
     .encode(x="가격등록일자:T", y=f"{PRICE_COL}:Q")
 )
 
 spike_down_chart = (
     alt.Chart(spike_down)
-    .mark_circle(size=55, color="rgba(30,80,255,0.60)")
+    .mark_circle(size=55, color="rgba(30,80,255,0.70)")
     .encode(x="가격등록일자:T", y=f"{PRICE_COL}:Q")
 )
 
